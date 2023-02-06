38730c1b2cba13198ab06a51cc88ed03511321c9
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-02-06 23:45:23 +0330

    init

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # source /home/saeed/anaconda3/etc/profile.d/conda.sh
 # conda activate deploy
-streamlit run "1_👁‍🗨_Doc_Insight.py" --server.address 0.0.0.0 --server.port 8080
+# streamlit run "1_👁‍🗨_Doc_Insight.py" --server.address 0.0.0.0 --server.port 8080
+streamlit run "1_👁‍🗨_Doc_Insight.py" --server.port 8501 --server.enableXsrfProtection false
